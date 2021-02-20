9a1b44154cc31242e5e31e95bde06dfa25cbaab6
Darox4241 <glebsilaev@gmail.com>
2021-02-20 17:15:57 +0300

    Add files via upload

---
+++ API.py
@@ -0,0 +1,192 @@
+from flask import Flask, request
+import json
+import simplejson
+import requests
+from operator import itemgetter
+import uuid
+
+base_url = 'http://127.0.0.1:5000'
+response = requests.get(base_url + '/scheme')
+response = response.json()
+
+sx = response['size']['size_x']
+sy = response['size']['size_y']
+sz = response['size']['size_z']
+
+merged_arr = response['merged']
+
+Pos = []
+num = 0
+
+app = Flask(__name__, static_folder='')
+
+
+
+@app.route('/', methods=['GET'])
+def mult():
+    if request.args['name'] != '':
+        a = [(request.args['name']),
+             (request.args['x']),
+             (request.args['y']),
+             (request.args['z']),
+             (request.args['mass'])]
+        a.reverse()
+        a.append(str(uuid.uuid4()))
+        a.reverse()
+        Pos.append(a)
+        return {"status": "ok"}, 200
+    else:
+        return {"status": "error"}, 405
+
+
+@app.route('/gettable', methods=['GET'])
+def get_table():
+    arr = [[0 for i in range(sx)] for j in range(sy)]
+    storg = []
+    remote = []
+    tkn = []
+
+    for i in merged_arr:
+        if len(i) == 4:
+            arr[int(i[0][1:len(i)]) - 1][int(ord(i[0][0]) - int(ord('A')))] = 3
+            for m in range(1, 4):
+                arr[int(i[m][1:len(i)]) - 1][int(ord(i[m][0]) - int(ord('A')))] = 4
+        else:
+            if i[0][0] == i[1][0]:
+                arr[int(i[0][1:len(i)]) - 1][int(ord(i[0][0]) - int(ord('A')))] = 1
+                arr[int(i[1][1:len(i)]) - 1][int(ord(i[1][0]) - int(ord('A')))] = 4
+            else:
+                arr[int(i[0][1:len(i)]) - 1][int(ord(i[0][0]) - int(ord('A')))] = 2
+                arr[int(i[0][1:len(i)]) - 1][int(ord(i[0][0]) - int(ord('A'))) + 1] = 4
+
+
+    for i in Pos:
+        px = int(i[2])
+        py = int(i[3])
+        pz = int(i[4])
+        A = []
+        A.append(px)
+        A.append(py)
+        A.append(pz)
+        mx = 0
+        for m in A:
+            if mx < m < 1000:
+                mx = m
+        try:
+            A.remove(mx)
+        except ValueError:
+            i.pop(3)
+            i.pop(3)
+            i[2] = 4
+            continue
+        px = A[0]
+        py = A[1]
+        i.pop(3)
+        i.pop(3)
+        if px > 2000 or py > 2000:
+            i[2] = 4
+        else:
+            if px > 1000 and py > 1000:
+                i[2] = 3
+            elif px <= 1000 and py <= 1000:
+                i[2] = 0
+            else:
+                i[2] = 1
+    Pos1 = Pos.copy()
+
+
+    Pos1.sort(key=itemgetter(2, 3), reverse=True)
+    for i in Pos1:
+            for y in range(sy - 1, -1, -1):
+                for x in range(sx - 1, -1, -1):
+                    cell = chr(x + ord('A')) + str(y + 1)
+                    if cell not in tkn and i[0] not in tkn:
+                        if arr[y][x] == i[2] == 3 or (
+                                i[2] == 1 and (arr[y][x] == 1 or arr[y][x] == 2 or arr[y][x] == 3)):
+                            storg.append([i[0], i[1], []])
+                            tkn.append(i[0])
+                            for mrg in merged_arr:
+                                try:
+                                    if mrg.index(cell) == 0:
+                                        for cells in mrg:
+                                            storg[-1][2].append(cells)
+                                            tkn.append(cells)
+                                except ValueError:
+                                    continue
+                        if i[2] == 0:
+                            if arr[y][x] == 1 or arr[y][x] == 2 or arr[y][x] == 3:
+                                storg.append([i[0], i[1], []])
+                                tkn.append(i[0])
+                                for mrg in merged_arr:
+                                    try:
+                                        if mrg.index(cell) == 0:
+                                            for cells in mrg:
+                                                storg[-1][2].append(cells)
+                                                tkn.append(cells)
+                                    except ValueError:
+                                        continue
+                            elif arr[y][x] == 0:
+                                storg.append([i[0], i[1], [cell]])
+                                tkn.append(cell)
+                                tkn.append(i[0])
+                                break
+            else:
+                if i[0] not in tkn:
+                    remote.append(i[1])
+                    tkn.append(i[0])
+
+    storg_site = []
+    for i in storg:
+        sndid = i[0]
+        snddst = i[2]
+        snd = [{
+            'uuid': sndid,
+            'destination': snddst,
+        }]
+        snd_json = simplejson.dumps(snd)
+        response = requests.post('http://127.0.0.1:5000/', snd_json, headers={"Content-Type": "application/json"})
+        response = response.json()
+        if response['status'] == 'ok':
+            storg_site.append([i[0], i[1], i[2]])
+    return json.dumps({
+        'sx': sx,
+        'sy': sy,
+        'sz': sz,
+        'arr': arr,
+        'storg_site': storg_site,
+        'stremote': remote,
+    }), 200
+
+
+@app.route('/position', methods=['POST'])
+def get_item():
+    inPut = request.args['dest']
+    inPut = list(map(str, inPut.split(',')))
+    if len(inPut) == 1:
+        dest = ({
+            "destination": [inPut[0]],
+        })
+        response = requests.get(base_url + '/position', dest)
+        response = response.json()
+        if response['status'] == 'ok':
+            return {"status": "ok"}
+        elif response['status'] == 'position is empty':
+            return {"status": "position is empty"}, 404
+        else:
+            return {"status": "position does not exist"}, 400
+    else:
+        dest = ({
+            "destination": [inPut[0]],
+        })
+        response = requests.get(base_url + '/position', dest)
+        response = response.json()
+        if response['status'] == 'ok':
+            return {"status": "ok"}
+        elif response['status'] == 'position is empty':
+            return {"status": "position is empty"}
+        else:
+            return {"status": "position does not exist"}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
---
+++ templates/MainFuncs.js
@@ -0,0 +1,211 @@
+function stp() {
+    var req = getXmlHttp()
+
+    var targetElem = document.getElementById('tableData')
+
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            if(req.status == 200) {
+                var res = JSON.parse(req.responseText);
+                sx = parseInt(res.sx);
+                sy = parseInt(res.sy);
+                sz = parseInt(res.sz);
+                arr = res.arr;
+                storg_site = res.storg_site;
+                stremote = res.stremote
+
+
+                txtremote = 'Отправлены на удаленный склад: '
+                for (var i = 0; i < stremote.length; i++) {
+                  txtremote = txtremote + stremote[i] + ', '
+                }
+                alert(txtremote);
+
+
+                (document.querySelector('input.name')).setAttribute('disabled', '');
+                (document.querySelector('input.x')).setAttribute('disabled', '');
+                (document.querySelector('input.y')).setAttribute('disabled', '');
+                (document.querySelector('input.z')).setAttribute('disabled', '');
+                (document.querySelector('input.mass')).setAttribute('disabled', '');
+                (document.getElementById('inputform')).setAttribute('hidden', 'true');
+
+                var infoLabel = document.createElement('table');
+
+                var tbl = document.createElement('table');
+                tbl.setAttribute('border', '2');
+                tbl.setAttribute('class', 'main_table');
+
+                var tbdy = document.createElement('tbody');
+
+                for (var i = 0; i < sy; i++) { // rows
+
+                    var tr = document.createElement('tr');
+                    tr.style.height = '100px';
+                    for (var j = 0; j < sx; j++) { // columns
+                        var td = document.createElement('td');
+                        td.style.width = '100px';
+                        if(arr[i][j] == 1) {
+                            td.setAttribute('rowspan', '2')
+                        }
+                        if(arr[i][j] == 2) {
+                            td.setAttribute('colspan', '2')
+                        }
+                        if(arr[i][j] == 3) {
+                            td.setAttribute('rowspan', '2');
+                            td.setAttribute('colspan', '2');
+                        }
+                        if(arr[i][j] == 4) {
+                            continue;
+                        }
+                        tr.appendChild(td)
+
+                    }
+                    tbdy.appendChild(tr);
+                }
+                tbl.appendChild(tbdy);
+                targetElem.appendChild(infoLabel);
+                targetElem.appendChild(tbl);
+
+                document.getElementById('div_storg').removeAttribute('hidden');
+                var storg = document.createElement('table');
+                var tr0 = document.createElement('tr');
+                var td = document.createElement('th');
+                var butt = document.createElement('button');
+                td.innerHTML = 'Название товара';
+                tr0.appendChild(td.cloneNode(true));
+                td.innerHTML = 'Занимаемые ячейки';
+                tr0.appendChild(td.cloneNode(true));
+                td.innerHTML = 'Нажмите, чтобы отправить запрос';
+                td.setAttribute('class', 'td');
+                tr0.appendChild(td.cloneNode(true));
+                storg.setAttribute('id', 'storg_table');
+                storg.appendChild(tr0.cloneNode(true));
+                document.getElementById('div_storg').appendChild(storg.cloneNode(true));
+
+
+
+                for (var i = 0; i < (storg_site.length); i++) {
+                  var td = document.createElement('td')
+                  td.setAttribute('class', 'td')
+                  var tr = document.createElement('tr')
+                  tr.setAttribute('class','td')
+                  td.innerHTML = (storg_site[i])[1]
+                  tr.appendChild(td.cloneNode(true))
+
+
+                  td.innerHTML = storg_site[i][2]
+                  tr.appendChild(td.cloneNode(true))
+
+
+                  var butt = document.createElement('button')
+                  butt.setAttribute('onclick', 'gtpos(this)')
+                  butt.setAttribute('value', 'Отправить запрос на выдачу')
+                  butt.setAttribute('class', 'get-button')
+                  td.innerHTML = ''
+                  td.appendChild(butt.cloneNode(true))
+                  tr.appendChild(td.cloneNode(true))
+
+                  document.getElementById('storg_table').appendChild(tr.cloneNode(true));
+                }
+
+            }
+        }
+      }
+
+    req.open('GET', '/gettable', true);
+
+    req.send(null);
+}
+function getXmlHttp(){
+      var xmlhttp;
+      try {
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+      } catch (e) {
+        try {
+          xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (E) {
+          xmlhttp = false;
+        }
+      }
+      if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+        xmlhttp = new XMLHttpRequest();
+      }
+      return xmlhttp;
+    }
+
+
+    function snd() {
+        if (((document.querySelector('input.name')).value != "") &&
+        ((document.querySelector('input.x')).value != "") &&
+        ((document.querySelector('input.y')).value != "") &&
+        ((document.querySelector('input.z')).value != "") &&
+        ((document.querySelector('input.mass')).value != "")) {
+
+        const nodet = document.createElement('tr');
+        var node = document.createElement('td');
+        document.getElementById('naklad').appendChild(nodet);
+
+        node.innerHTML = (document.querySelector('input.name')).value;
+        nodet.appendChild(node.cloneNode(true));
+
+
+        node.innerHTML = (document.querySelector('input.x')).value;
+        nodet.appendChild(node.cloneNode(true));
+
+
+        node.innerHTML = (document.querySelector('input.y')).value;
+        nodet.appendChild(node.cloneNode(true));
+
+
+        node.innerHTML = (document.querySelector('input.z')).value;
+        nodet.appendChild(node.cloneNode(true));
+
+
+        node.innerHTML = (document.querySelector('input.mass')).value;
+        nodet.appendChild(node.cloneNode(true));
+
+        let a = 'http://127.0.0.1:3000/?name=' + document.querySelector('input.name').value +
+                    '&x=' + document.querySelector('input.x').value +
+                    '&y=' + document.querySelector('input.y').value +
+                    '&z=' + document.querySelector('input.z').value +
+                    '&mass=' + document.querySelector('input.mass').value
+        var request = new XMLHttpRequest();
+        request.open('get', a, true)
+        request.send()
+
+       }
+       else {
+       alert('Заполните все поля!')
+       }
+      }
+
+
+
+function gtpos(ths) {
+    tr = ths.parentNode.parentNode
+    getdest = tr.getElementsByTagName('td')[1].innerHTML
+    var inPut = getdest.split(', ')
+    if (inPut != []) {
+    request = new XMLHttpRequest();
+    console.log(inPut)
+    request.open('post', '/position?dest=' + inPut, true);
+
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            if(request.status == '200') {
+                alert('Товар выдан успешно!')
+            }
+            if(request.status == '404') {
+                alert('Введенная ячейка пуста')
+            }
+            if(request.status == "400") {
+                alert('Введенной ячейки не существует')
+            }
+
+    }
+    }
+    request.send();
+    tr.setAttribute('hidden', '')
+   }
+}
